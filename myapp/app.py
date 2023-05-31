67337fc6f362e17e900167d1eec2ea80dc62ebf7
channacy <68759286+channacy@users.noreply.github.com>
2023-05-31 00:01:51 -0400

    removed jupyter notebooks and imported models

--- myapp/app.py
+++ myapp/app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, LoginManager, login_required, login_user, current_user
+from datetime import datetime
+from authlib.integrations.flask_client import OAuth
+import pickle 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+oauth = OAuth(app)
+
+with open('/Users/channacyun/LivelyTweet/myapp/tweet_location.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -18,6 +26,39 @@ class Todo(db.Model):
 class TodoForm(FlaskForm):
     todo = StringField("Todo")
     submit = SubmitField('Add Todo')
+
+# create login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    password_hash = db.Column(db.String(128))
+    joined_at = db.Column(db.DateTime(), index = True, default = datetime.utcnow)
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+class RegistrationForm(FlaskForm):
+  username = StringField('Username', validators=[DataRequired()])
+  # add email field here:
+  email = StringField('Email', validators = [DataRequired(), Email()])
+  # add password fields here:
+  password = PasswordField('Password', validators = [DataRequired()])
+  password2 = PasswordField('Repeat Password', validators = [DataRequired(), EqualTo('password')])
+
+  submit = SubmitField('Register')
+
+# login form
+class LoginForm(FlaskForm):
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  remember = BooleanField('Remember Me')
+  submit = SubmitField('Login')
 
 with app.app_context():
     db.create_all()
@@ -28,3 +69,74 @@ def index():
         db.session.add(Todo(todo_text = request.form['todo']))
         db.session.commit()
     return render_template('index.html', todos = Todo.query.all(), template_form = TodoForm())
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  form = RegistrationForm(csrf_enabled=False)
+  if form.validate_on_submit():
+    # define user with data from form here:
+    user = User(username = form.username.data, email = form.email.data)
+    # set user's password here:
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    return redirect("/")
+  return render_template('register.html', title='Register', form=form)
+
+# user loader
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
+# login route
+@app.route('/login', methods=['GET','POST'])
+def login():
+  form = LoginForm(csrf_enabled=False)
+  if form.validate_on_submit():
+    # query User here:
+    user = User.query.filter_by(email = form.email.data).first()
+    # check if a user was found and the form password matches here:
+    if user and user.check_password(form.password.data):
+      # login user here:
+      login_user(user, remember = form.remember.data)
+      next_page = request.args.get('next')
+      return redirect(next_page) if next_page else redirect(url_for('/', _external=True, _scheme='https'))
+    else:
+      return redirect(url_for('login', _external=True, _scheme='https'))
+  return render_template('login.html', form=form)
+
+# user route
+@app.route('/user/<username>')
+@login_required
+def user(username):
+  user = User.query.filter_by(username=username).first_or_404()
+  return render_template('user.html', user=user)
+
+@app.route('/twitter/')
+def twitter():
+    # Twitter Oauth Config
+    TWITTER_CLIENT_ID = "xwHg79CTHMYVgeXvdF3EqPAdc"
+    TWITTER_CLIENT_SECRET = "MEPG2pTaciRDqD5kSmAENNzL9HKXoKYDO9gX6Ir1j4jZmo8m3H"
+    oauth.register(
+        name='twitter',
+        client_id=TWITTER_CLIENT_ID,
+        client_secret=TWITTER_CLIENT_SECRET,
+        request_token_url='https://api.twitter.com/oauth/request_token',
+        request_token_params=None,
+        access_token_url='https://api.twitter.com/oauth/access_token',
+        access_token_params=None,
+        authorize_url='https://api.twitter.com/oauth/authenticate',
+        authorize_params=None,
+        api_base_url='https://api.twitter.com/1.1/',
+        client_kwargs=None,
+    )
+    redirect_uri = url_for('twitter_auth', _external=True)
+    return oauth.twitter.authorize_redirect(redirect_uri)
+ 
+@app.route('/twitter/auth/')
+def twitter_auth():
+    token = oauth.twitter.authorize_access_token()
+    resp = oauth.twitter.get('account/verify_credentials.json')
+    profile = resp.json()
+    print(" Twitter User", profile)
+    return redirect('/')
